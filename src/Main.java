0cf596b4bdc4a0be603530983a809b791128a113
Antisynrgy <s.e.mansel@web.de>
2026-06-16 23:20:24 +0200

    Kurs 1.0: Variablen: Einfach Kombination (sum)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //Variable: Ein Speicher für einen Wert
-        //Datentyp: Gibt an, von welcher Art die Variable ist
-
         int number = 3;
-        System.out.println(number);
+        int number2 = 14;
+        int sum = number + number2;
+        System.out.println(sum);
 
     }
 }
