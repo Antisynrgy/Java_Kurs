7094123b6959fa1ad760be97243adaa6a85fb7f7
Antisynrgy <s.e.mansel@web.de>
2026-06-16 23:18:31 +0200

    Kurs 1.0: Variablen Einführung

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!²");
-        System.out.println("Hello World!³ ❤❤❤");
+        //Variable: Ein Speicher für einen Wert
+        //Datentyp: Gibt an, von welcher Art die Variable ist
+
+        int number = 3;
+        System.out.println(number);
+
     }
 }
