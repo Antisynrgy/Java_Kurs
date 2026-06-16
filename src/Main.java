a0e4fdf3d72b2f85c265e8510f4ea748e06545a9
Antisynrgy <s.e.mansel@web.de>
2026-06-16 23:27:50 +0200

    Kurs 1.0: Variablen: Bsp. Birnen

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int apples = 2435;
-        String text = "Ich habe so viele Äpfel: ";
-        System.out.println("Ich habe so viele Äpfel: " + apples);
-        System.out.println(text + apples);
+        int anzahl = 12;
+        String obst = " Birnen";
+        System.out.println("Ich möchte " + anzahl + obst);
+
 
     }
 }
