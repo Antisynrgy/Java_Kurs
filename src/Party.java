bbe93fa4a939f6baec789aaf7150a5fdf91293a6
Antisynrgy <s.e.mansel@web.de>
2026-06-17 23:42:56 +0200

    Kurs 1.0: Variablen: Kurzschreibweisen - Bsp. 1 Gästeliste

--- src/Party.java
+++ src/Party.java
@@ -2,8 +2,11 @@ public class Party {
     public static void main(String[] args) {
         int guests = 11;
 
-        guests = guests -1;  //eine Absage       //Neue Zuweisung überschreibt vorherige
-        guests = guests + 3; //drei Zusagen
+        guests -= 1;            //entspricht: guests = guests -1; oder guests--;
+
+        System.out.println("Anzahl der Gäste, die kommen: " + guests);
+
+        guests += 3;
 
         System.out.println("Anzahl der Gäste, die kommen: " + guests);
     }
