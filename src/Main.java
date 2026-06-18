06dd51a8f41c669303b973eb2756b740d361d842
Antisynrgy <s.e.mansel@web.de>
2026-06-18 23:00:10 +0200

    Kurs 1.0: IF ELSE - Bsp.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        if (Bedingung) {        //IF ELSE: Wenn Bedingung erfüllt --> IF-Code ausführen, wenn nicht, dann ELSE-Code
-
+        int number = 5;             //= : Zuweisung, == : Test(Gleich), != : Test(Ungleich), >,<,>=,<=
+        if (number == 5) {
+            System.out.println("Number ist gleich 5");
         } else {
+            System.out.println("Number ungleich 5");
+        }
 
-        }
-        }
     }
 }
