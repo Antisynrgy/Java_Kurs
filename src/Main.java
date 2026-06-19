b70a78c69e8e79e03c1885ebe54deecabbaee5d0
Antisynrgy <s.e.mansel@web.de>
2026-06-19 23:51:32 +0200

    Kurs 1.0: Logische Operatoren: Nicht !

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        //Übung: Alter > 18 --> Volljährig, sonst Minderjährig
-        int number = 19;
-        boolean Alter = (number >= 18);
-        if (Alter) {
-            System.out.println("Volljährig");
-        } else {
-            System.out.println("Minderjährig");
-        }
+        double temperatur = 18;
 
+        if (!(temperatur > 20)) {           // ! : Nicht-Operator --> (!(temperatur > 20)) = (temperatur <= 20)
+            System.out.println("Kalt");
+        } else {
+            System.out.println("Angenehm");
+        }
     }
 }
