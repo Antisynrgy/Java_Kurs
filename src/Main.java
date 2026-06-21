08ce0034b742a6e8f825447f686311edb3395f05
Antisynrgy <s.e.mansel@web.de>
2026-06-21 23:22:18 +0200

    Kurs 1.0: While-Schleife: While Schleife + Scanner Eingabe

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int counter = 10;
-        while(true) {
-            System.out.println("Der Counter steht bei " + counter);
-            counter--;
-            if (counter < 0) {
-                break;
-            }
+        Scanner scan = new Scanner(System.in);
+        int value = 1;
+
+        while(value == 1) {
+            System.out.println("All I need ");
+            System.out.println("Is your love tonight");
+            System.out.println("Erneut abspielen? ja(1) nein(2)");
+            value = scan.nextInt();
+
         }
         System.out.println("Programm beendet");
     }
