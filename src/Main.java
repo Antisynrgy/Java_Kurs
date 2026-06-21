050399a5d7cf548cf5ccee3ce6b27dbe60272a5d
Antisynrgy <s.e.mansel@web.de>
2026-06-21 23:11:46 +0200

    Kurs 1.0: While-Schleife: While-Schleife + boolean

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int counter = 10;
-        //while-Schleife läuft so lange, bis Bedingung nicht mehr erfüllt ist
-        while(counter >= 0) {
+        boolean weiter = true;
+        while(weiter) {
             System.out.println("Der Counter steht bei " + counter);
             counter--;
+            if (counter < 0) {
+                weiter = false;
+            }
         }
+        System.out.println("Programm beendet");
     }
 }
