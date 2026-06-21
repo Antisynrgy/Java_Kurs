ff73ab64ff14a86864c6f5bd05476c87f773b601
Antisynrgy <s.e.mansel@web.de>
2026-06-21 23:31:39 +0200

    Kurs 1.0: While-Schleife: Bsp.: Anzahl Partygäste

--- src/Party.java
+++ src/Party.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 public class Party {
     public static void main(String[] args) {
-        int guests = 11;
+        int guests;
+        Scanner scan = new Scanner(System.in);
+        boolean weiter = true;
 
-        guests -= 1;            //entspricht: guests = guests -1; oder guests--;
+        System.out.println("Anzahl der angemeldeten Gäste: ");
+        guests = scan.nextInt();
 
-        System.out.println("Anzahl der Gäste, die kommen: " + guests);
+        while(weiter) {
+           System.out.println("Gast anmelden (1), abmelden (2), Programm beenden (3)");
+           int value = scan.nextInt();
 
-        guests += 3;
-
-        System.out.println("Anzahl der Gäste, die kommen: " + guests);
+           if (value == 1) {
+               guests++;
+           }
+           if (value == 2) {
+                guests--;
+            }
+           if (value == 3) {
+                weiter = false;
+            }
+        }
+        System.out.println("Anzahl der Gäste: " + guests);
     }
 }
