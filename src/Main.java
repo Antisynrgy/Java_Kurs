77dbe72065500cad96f6d6d6d50b5eced73275be
Antisynrgy <s.e.mansel@web.de>
2026-06-23 00:17:37 +0200

    Kurs 1.0: For-Schleife: Bsp. 1 mit Nutzereingabe

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //For: Code x mal ausführen --> keine Abbruchbedingung notwendig
-        //int i = 0; --> Erstellung Variable
-        // i < 5; --> Anzahl der Durchläufe
-        // i++ oder i += 3 --> Schrittgröße beim Zählen der Durchläufe (Zählweite/Schrittweite)
-        for (int i = 0; i < 5; i++) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Wie viele Zahlen möchtest du?");
+        int value = scan.nextInt();
+        for (int i = 0; i < value; i++) {
             System.out.println(i);
         }
     }
