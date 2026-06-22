06adf3eb7d0045a7fe0f93fcab73da68ee6aea3d
Antisynrgy <s.e.mansel@web.de>
2026-06-23 00:27:34 +0200

    Kurs 1.0: For-Schleife: Bsp. 2 Übung - Nutzereingabe Start- & Endwert

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Gib einen Startwert ein!");
+        int Start = scan.nextInt();
 
-        for (int i = 50; i < 100; i += 2) {
+        System.out.println("Gib einen Endwert ein!");
+        int End = scan.nextInt();
+
+        for (int i = Start; i < End; i++) {
             System.out.println(i);
         }
     }
