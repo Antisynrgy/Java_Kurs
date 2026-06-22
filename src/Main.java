ef678a29f70e17a74e9175da172a3ed702346e38
Antisynrgy <s.e.mansel@web.de>
2026-06-23 00:15:10 +0200

    Kurs 1.0: For-Schleife: Aufbau/Erklärung

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int value = 1;
-
-        while(value == 1) {
-            System.out.println("All I need ");
-            System.out.println("Is your love tonight");
-            System.out.println("Erneut abspielen? ja(1) nein(2)");
-            value = scan.nextInt();
-
+        //For: Code x mal ausführen --> keine Abbruchbedingung notwendig
+        //int i = 0; --> Erstellung Variable
+        // i < 5; --> Anzahl der Durchläufe
+        // i++ oder i += 3 --> Schrittgröße beim Zählen der Durchläufe (Zählweite/Schrittweite)
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
         }
-        System.out.println("Programm beendet");
     }
 }
