b1c620c16722bf9cc333af433801a92f2c945d60
Antisynrgy <s.e.mansel@web.de>
2026-06-24 00:11:59 +0200

    Kurs 1.0: For-Schleife: Bsp. 3 - Countdown

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Gib einen Startwert ein!");
-        int Start = scan.nextInt();
 
-        System.out.println("Gib einen Endwert ein!");
-        int End = scan.nextInt();
-
-        for (int i = Start; i < End; i++) {
-            System.out.println(i);
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Der Counter steht bei " + i);
         }
     }
 }
